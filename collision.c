79f23a4accba07b8ddafcf450c1b06aa7fb08360
nexuslrf <shootingstarlrf@hotmail.com>
2019-09-23 12:42:21 +0800

    Finish narrow collision detection

--- collision.c
+++ collision.c
@@ -6,6 +6,7 @@
 #include <time.h>
 #include <xmmintrin.h>
 #include <string.h>
+#include <math.h>
 
 #define RAND01 (rand()%2)
 
@@ -23,6 +24,28 @@ typedef struct
     double vy_n;
     /////////////////
 }  Particle;
+typedef struct
+{
+    int pa;
+    int pb;
+    double time;
+} Collision;
+int compare (const void * a, const void * b)
+{
+    Collision *colli_A = a;
+    Collision *colli_B = b;
+    double cmpf = colli_B->time - colli_A->time;
+    if(cmpf!=0)
+        return cmpf<0?-1:1;
+    else
+    {
+        int cmpt = colli_B->pa - colli_A->pa;
+        if(cmpt!=0)
+            return cmpt;
+        else
+            return colli_B->pb - colli_A->pb;
+    }
+}
 int N, L, r, S;
 char mode[6];
 
@@ -44,12 +67,13 @@ double bound_pos(double p)
 int main()
 {
     srand(0);
-    Particle *particles;
+    Particle *particles, *P_a, *P_b;
     freopen("./inputs.txt","r",stdin);
     scanf("%d %d %d %d %s",&N, &L, &r, &S, mode);
     particles = (Particle *)malloc(N * sizeof(Particle));
-    int i =0,j, t, idx;
-    double x, y, vx, vy;
+    int i =0,j, t, idx, cnt;
+    double x, y, vx, vy, lambda, lambda_1, lambda_2;
+    double dx1, dx2, dy1, dy2, Dx, Dy, DDpDD, dDpdD, dDmdD, Delta;
     while(scanf("%d %lf %lf %lf %lf", &idx,&x,&y,&vx,&vy)!=EOF)
     {
         i++;
@@ -83,24 +107,104 @@ int main()
     2. Build collision time table for processing.
     🎯
     */
-    int **colli_mat;
-    colli_mat = (int**)malloc((N+1)*sizeof(int*));
-    for(i=0; i<N+1; i++)
-    {    
-        colli_mat[i]=(int*)malloc((N+1)*sizeof(int));
-        memset(colli_mat[i],0,(N+1)*sizeof(int));
-    }
-    double *colli_time = (double*)malloc(N*(N+1)/2*sizeof(double));
+
+    // int **colli_mat;
+    // colli_mat = (int**)malloc((N+1)*sizeof(int*));
+    // for(i=0; i<N+1; i++)
+    // {    
+    //     colli_mat[i]=(int*)malloc((N+1)*sizeof(int));
+    //     memset(colli_mat[i],0,(N+1)*sizeof(int));
+    // }
+    int *colli_mat = (int *)malloc(N*sizeof(int));
+    memset(colli_mat,0, N*sizeof(int));
+    Collision *colli_time = (Collision*)malloc(N*(N+1)/2*sizeof(Collision));
     // Start sim
+    double r_sq_4 = 4*r*r;
     for(t=0;t<S;t++)
     {
         // Step 1: speculate no collision happen, get new pos & v.
         for(i=0; i<N; i++)
         {
-            particles[i].x_n = bound_pos(particles[i].x + particles[i].vx);
-            particles[i].y_n = bound_pos(particles[i].y + particles[i].vy);
+            particles[i].x_n = particles[i].x + particles[i].vx;
+            particles[i].y_n = particles[i].y + particles[i].vy;
         }
         // Step 2: find all possible collision independently. fill colli_mat and colli_time.
+        cnt = 0;
+        for(i=0; i<N; i++)
+        {
+            P_a = particles+i;
+            //Case 1: collision with wall
+            ///////////////
+            lambda_1 = lambda_2 = 2;
+            if(P_a->x_n<r)
+            {
+                lambda_1 = (r - P_a->x) / P_a->vx;
+            }
+            else if(P_a->x_n>L-r)
+            {
+                lambda_1 = (L-r - P_a->x) / P_a->vx;
+            }
+
+            if(P_a->y_n<r)
+            {
+                lambda_2 = (r - P_a->y) / P_a->vy;
+            }
+            else if(P_a->y_n>L-r)
+            {
+                lambda_2 = (L-r - P_a->y) / P_a->vy;
+            }
+            lambda = lambda_1 < lambda_2? lambda_1:lambda_2;
+            if(lambda < 1)
+            {
+                colli_time[cnt].time = lambda;
+                colli_time[cnt].pa = i;
+                colli_time[cnt].pb = N;
+                cnt++;
+            }
+            ///////////////
+            for(j=i+1; i<N; i++)
+            {
+                P_b = particles+j;
+                // Case 2: overlap at startup
+                ////////////////
+                dx1 = P_b->x - P_a->x;
+                dy1 = P_b->y - P_a->y;
+                if(dx1*dx1 + dy1*dy1 - r_sq_4<=0)
+                {
+                    colli_time[cnt].time = 0;
+                    colli_time[cnt].pa = i;
+                    colli_time[cnt].pb = j;
+                    cnt++;
+                    break; // no need to further detect.
+                }
+                ////////////////
+                // Case 3: Normal collision case
+                ////////////////
+                dx2 = P_b->x_n - P_a->x_n;
+                dy2 = P_b->y_n - P_a->y_n;
+                Dx = dx2 - dx1;
+                Dy = dy2 - dy1;
+                DDpDD = Dx*Dx + Dy*Dy;
+                dDmdD = dx1*Dy - dy1*Dx;
+                Delta = r_sq_4*DDpDD - dDmdD*dDmdD;
+                dDpdD = dx1*Dx + dy1*Dy;
+                if(Delta<=0||dDpdD>0)
+                    continue;
+                Delta = sqrt(Delta);
+                lambda_1 = (-dDpdD + Delta)/DDpDD;
+                lambda_2 = (-dDpdD - Delta)/DDpDD;
+                lambda = lambda_1<lambda_2?lambda_1:lambda_2;
+                if(lambda<1)
+                {
+                    colli_time[cnt].time = lambda;
+                    colli_time[cnt].pa = i;
+                    colli_time[cnt].pb = j;
+                    cnt++;
+                }
+                ////////////////
+            }
+        }
+        // Step 3: sort collision table and process collision
         
 
     }
@@ -108,5 +212,7 @@ int main()
 
     fclose(stdin);
     free(particles);
+    free(colli_time);
+    free(colli_mat);
     return 0;
 }
